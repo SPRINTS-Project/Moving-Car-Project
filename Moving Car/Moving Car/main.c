4d8891dcce8584aa6d94eb3fa383ecbc298e8fc2
Matarawy <mahmoudsarhan02672@gmail.com>
2023-04-15 19:41:18 +0200

    update
    fix reload project problem

---
+++ Moving Car/Moving Car/main.c
@@ -0,0 +1,16 @@
+/*
+ * Moving Car.c
+ *
+ * Created: 4/9/2023 8:13:03 PM
+ * Author : Mahmoud Sarhan
+ *	EMAIL : Eng.mahmoud.adel94@gmail.com
+ */ 
+
+#include "APP/app.h"
+
+
+int main(void)
+{
+	APP_start();
+}
+
